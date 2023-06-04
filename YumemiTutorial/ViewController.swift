e99ba1beb454abf4639cc335fe1c72123aafa11e
itoko <0405ikss@gmail.com>
2023-06-04 19:09:20 +0900

    session 2 APIの利用
    YumemiWeatherを利用して天候状況を取得して画像として表示する機能を追加

--- YumemiTutorial/ViewController.swift
+++ YumemiTutorial/ViewController.swift
@@ -6,13 +6,40 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
+    private var weather:String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setWeatherImage()
     }
+    
+    private func setWeatherImage() {
+        weather = YumemiWeather.fetchWeatherCondition()
+        let name:String = weather
+        var color:UIColor
+        if weather == "sunny" {
+            color = .red
+        } else if weather == "cloudy" {
+            color = .gray
+        } else {
+            color = .blue
+        }
+        weatherImageView.image = UIImage(named: name)?.withRenderingMode(.alwaysTemplate)
+        weatherImageView.tintColor = color
+    }
+ 
+    // Actions
+    @IBAction func tapReloadButton(_ sender: Any) {
+        setWeatherImage()
+    }
+    
 
 
 }
