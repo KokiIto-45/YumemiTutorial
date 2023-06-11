21ee4d05cdb4223527f509b0ee4f96d7ffbb8867
itoko <0405ikss@gmail.com>
2023-06-11 17:50:52 +0900

    session/2 天気予報を取得し、表示する
    変数宣言の表記を修正
    weatherの情報をグローバルで持たないように修正
    Weatherクラスを作成し、sunny,cloudyなどのconditionをEnum型で保持、また各conditionごとのimageColorも同時に保持するように修正
    また、setWeatherImage関数を、getWeatherConditionViaAPIとsetWeatherImageの二つに分離し、setWeatherImageはWeatehr型さえ用意すれば任意のタイミングで画像を変更できるように修正

--- YumemiTutorial/ViewController.swift
+++ YumemiTutorial/ViewController.swift
@@ -12,32 +12,51 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var weatherImageView: UIImageView!
     
-    private var weather:String = ""
+    class Weather {
+        enum WeatherEnum: String {
+            case sunny = "sunny"
+            case cloudy = "cloudy"
+            case rain = "rainy"
+        }
+        let condition: WeatherEnum
+        let imageColor: UIColor
+        
+        init(weatherString: String) {
+            self.condition = WeatherEnum(rawValue: weatherString)!
+            if self.condition == .sunny {
+                self.imageColor = .red
+            } else if self.condition == .cloudy {
+                self.imageColor = .gray
+            } else {
+                self.imageColor = .blue
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        setWeatherImage()
+        guard let weather = getWeatherConditionViaAPI() else { return }
+        setWeatherImage(weather: weather)
     }
     
-    private func setWeatherImage() {
-        weather = YumemiWeather.fetchWeatherCondition()
-        let name:String = weather
-        var color:UIColor
-        if weather == "sunny" {
-            color = .red
-        } else if weather == "cloudy" {
-            color = .gray
-        } else {
-            color = .blue
-        }
+    private func getWeatherConditionViaAPI() -> Weather? {
+        let weatherString = YumemiWeather.fetchWeatherCondition()
+        let weather = Weather(weatherString: weatherString)
+        return weather
+    }
+    
+    private func setWeatherImage(weather: Weather) {
+        let name = weather.condition.rawValue
+        let color = weather.imageColor
         weatherImageView.image = UIImage(named: name)?.withRenderingMode(.alwaysTemplate)
         weatherImageView.tintColor = color
     }
  
     // Actions
     @IBAction func tapReloadButton(_ sender: Any) {
-        setWeatherImage()
+        guard let weather = getWeatherConditionViaAPI() else { return }
+        setWeatherImage(weather: weather)
     }
     
 
